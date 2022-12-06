c8ea1a0594c2b181094c7d67de086f776b46efb5
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-12-06 02:28:15 -0800

    added day6

---
+++ 2022/day6/main.py
@@ -0,0 +1,20 @@
+INPUT = 'input'
+
+def extract_input(filename):
+  file = open(filename, 'r')
+  return file.read()
+
+def find_marker(line, bufsize):
+  for i in range(bufsize, len(line)):
+    buff = line[i-bufsize: i]
+    if len(set(buff)) >= bufsize:
+      return i
+
+
+def main():
+  line = extract_input(INPUT)
+  print("first answer:", find_marker(line, 4))
+  print("second answer:", find_marker(line, 14))
+
+if __name__ == '__main__':
+  main()
