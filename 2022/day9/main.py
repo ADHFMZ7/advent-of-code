d9012c9ee5cde2ce933218a05a9e6f8130e72c5b
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-12-09 11:22:41 -0800

    finished day 9

--- 2022/day9/main.py
+++ 2022/day9/main.py
@@ -1,5 +1,13 @@
 INPUT = 'input'
 
+offsets = {
+  'R' : (0, 1),
+  'L' : (0, -1),
+  'U' : (1, 0),
+  'D' : (-1, 0)
+}
+
+
 def extract_input(filename):
   file = open(filename, 'r')
   return list(file.read().splitlines())
@@ -9,43 +17,35 @@ def touching(head, tail):
     return False
   return True
 
+def move_knot(head, tail):
+  if touching(head, tail):
+    return
+  for i in range(2):
+    if head[i] == tail[i]:
+      a = 1 if head[i] > tail[i] else -1
+      tail[i] += 1
+
+  if head[0] != tail[0] or head[1] != tail[1]:
+    a = 1 if head[0] > tail[0] else -1
+    b = 1 if head[1] > tail[1] else -1
+    tail[0] += a; tail[1] += b
+  return
+
 def main():
 
   lines = extract_input(INPUT)
-
-  head, tail = [0, 0], [0, 0]
-  tail_visit = set((0, 0)) 
+  knots = [[0, 0] for i in range(10)]
+  tail_visit = set() 
 
   for line in lines:
     direction, amount = line.split()
-    if direction == 'R':
-      offset = (0, 1)
-    elif direction == 'L':
-      offset = (0, -1)
-    elif direction == 'U':
-      offset = (1, 0)
-    elif direction == 'D':
-      offset = (-1, 0)
+    offset = offsets[direction]
     for i in range(int(amount)):
+      tail_visit.add(tuple(knots[-1]))
 
-      head[0] += offset[0]; head[1] += offset[1]
-
-      if head[0] == tail[0] and not touching(head, tail):
-        a = 1 if head[1] > tail[1] else -1
-        tail[1] += a
-        tail_visit.add(tuple(tail))
-
-      elif head[1] == tail[1] and not touching(head, tail):
-        a = 1 if head[0] > tail[0] else -1
-        tail[0] += a
-        tail_visit.add(tuple(tail))
-
-      elif not touching(head, tail) and (head[0] != tail[0] or head[1] != tail[1]):
-        a = 1 if head[0] > tail[0] else -1
-        b = 1 if head[1] > tail[1] else -1
-        tail[0] += a; tail[1] += b
-        tail_visit.add(tuple(tail))
-
+      knots[0][0] += offset[0]; knots[0][1] += offset[1]
+      for i in range(len(knots)-1):
+        move_knot(knots[i], knots[i+1])
   print(len(tail_visit))
 
 if __name__ == '__main__':
