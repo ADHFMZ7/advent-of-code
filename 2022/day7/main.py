d29c561442800ea509e624838debd414842a9409
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-12-09 18:49:56 -0800

    completed day 7

--- 2022/day7/main.py
+++ 2022/day7/main.py
@@ -6,22 +6,22 @@ def extract_input(filename):
 
 class directory:
 
-  def __init__(self, name='/', prev=None):
+  def __init__(self, name='/', prev='/'):
     self.name = name
     self.subdirs = []
     self.size = 0
-    self.prev = prev 
+    self.prev = prev if prev != '/' else self
     
   def ls(self, index, lines):
     index += 1
     while index != len(lines) and lines[index][0] != '$':
       arg1, arg2 = lines[index].split()
       if arg1 == 'dir':
+        print(f"Added directory {arg2}")
         self.add_directory(arg2)
-        #print(f"added directory {arg2}")
       else:
+        print(f"Added file of size {arg1} called {arg2}")
         self.size += int(arg1)
-        #print(f"added file {arg2} of size {arg1}")
       index += 1
 
   def compute_size(self):
@@ -29,14 +29,14 @@ class directory:
       return self.size
     res = 0
     for i in self.subdirs:
-      temp = i.compute_size()
-      res += temp
+      res += i.compute_size()
     self.size += res
     return self.size
 
   def add_directory(self, name):
-    temp = directory(name, self)
-    self.subdirs.append(temp) 
+    temp = directory(name, self) 
+    self.subdirs.append(temp)
+    return temp
 
   def cd(self, new):
     if new == '..':
@@ -44,14 +44,28 @@ class directory:
     for i in self.subdirs:
       if i.name == new:
         return i
+    return self.add_directory(new)
+
 
 def final_score(root):
-  root.compute_size()
-  curr = root
-  counter = 0
-  while curr:
-    if curr.size <= 
+  score = 0
+  score += root.size if root.size <= 100000 else 0
+  print(root.name, root.size)
+  for i in root.subdirs:
+    score += final_score(i)
+  return score
 
+
+def final_score2(root):
+  target = root.size - 40000000
+  def bfs(root):
+    valid_dirs = []
+    if root.size >= target:
+      valid_dirs.append(root.size)
+    for i in root.subdirs:
+      valid_dirs += bfs(i)
+    return valid_dirs
+  return min(bfs(root))
 
 def main():
   lines = extract_input(INPUT)
@@ -60,25 +74,18 @@ def main():
   root.add_directory('/')
   curr = root
   
-  for i, line in enumerate(lines[:50]):
-    #print(line)
+  for i, line in enumerate(lines):
+    print(i, line)
     if line[0] == "$":
       if 'ls' in line:
         curr.ls(i, lines)
       elif 'cd' in line:
         curr = curr.cd(line.split()[2])
+        print(f"  Changed into directory {curr.name}")
   root.compute_size()
-
-
-  def dfs(root):
-    print(root.name, ': ', root.size)
-    res = 0
-    for i in root.subdirs:
-      res += dfs(i)
-    
-  dfs(root)
-
-  #print(f"Total size is {final_score(root)}")
+  
+  print(f"Total size is {final_score(root)}")
+  print(f"The smallest file that will work is {final_score2(root)}")
 
 if __name__ == '__main__':
   main()
